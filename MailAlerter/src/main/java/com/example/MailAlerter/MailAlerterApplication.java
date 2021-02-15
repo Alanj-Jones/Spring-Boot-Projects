fbe7007a315a458acd6388e12aa080c1b30f45ae
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-02-14 23:54:13 -0300

    Final Mail Alerter

---
+++ MailAlerter/src/main/java/com/example/MailAlerter/MailAlerterApplication.java
@@ -0,0 +1,13 @@
+package com.example.MailAlerter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailAlerterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailAlerterApplication.class, args);
+	}
+
+}
---
+++ MailAlerter/src/test/java/com/example/MailAlerter/MailAlerterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MailAlerter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailAlerterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+ 
+}
